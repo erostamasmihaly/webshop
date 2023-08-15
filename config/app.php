e3a8d1c7600ec3a80f59c813e108e2fe6cdfa114
sa_et <eros.tamas@miskolcholding.hu>
2023-08-15 15:38:53 +0200

    Felhasználó beállítás

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'ETM WebShop'),
 
     /*
     |--------------------------------------------------------------------------
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'production'),
+    'env' => env('APP_ENV', 'local'),
 
     /*
     |--------------------------------------------------------------------------
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://webshop.etm'),
 
     'asset_url' => env('ASSET_URL'),
 
@@ -83,7 +83,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'hu',
 
     /*
     |--------------------------------------------------------------------------
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,58 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\User;
+use App\Models\UserRole;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Ha nincs még Admin felhasználó, akkor létrehozni
+        $has_admin = User::where("email","admin@etm.hu")->first();
+        if (!$has_admin) {
+            $user = new User();
+            $user->name = "admin";
+            $user->email = "admin@etm.hu";
+            $user->password = Hash::make("Admin1234");
+            $user->save();
+        } 
+        
+        // Ha nincs még Admin szerepkör, akkor létrehozni
+        $has_admin_role = Role::where("name","admin")->first();
+        if (!$has_admin_role) {
+            $role = new Role();
+            $role->name = "admin";
+            $role->save();
+        }
+
+        // Ha nincs még Buyer szerepkör, akkor létrehozni
+        $has_buyer_role = Role::where("name","buyer")->first();
+        if (!$has_buyer_role) {
+            $role = new Role();
+            $role->name = "buyer";
+            $role->save();
+        }
+
+        // Ha nincs még Seller szerepkör, akkor létrehozni
+        $has_seller_role = Role::where("name","seller")->first();
+        if (!$has_seller_role) {
+            $role = new Role();
+            $role->name = "seller";
+            $role->save();
+        }
+
+        // Ha az Admin nincs hozzárendelve az Admin szerepkörhöz
+        $has_admin_to_admin_role = UserRole::where("user_id",1)->where("role_id",1)->first();
+        if (!$has_admin_to_admin_role) {
+            $user_role = new UserRole();
+            $user_role->user_id = 1;
+            $user_role->role_id = 1;
+            $user_role->save();
+        }
     }
 }
