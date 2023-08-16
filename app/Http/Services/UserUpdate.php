8c3c354e1ff8ca9277304595f9e50c7d40d50136
sa_et <eros.tamas@miskolcholding.hu>
2023-08-16 14:24:51 +0200

    Felhasználók V.

--- app/Http/Services/UserUpdate.php
+++ app/Http/Services/UserUpdate.php
@@ -4,6 +4,7 @@ namespace App\Http\Services;
 
 use App\Http\Requests\UserUpdateRequest;
 use App\Models\User;
+use App\Models\UserRole;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
@@ -25,8 +26,36 @@ class UserUpdate {
     // Szerepkörök frissítése
     private function updateRoles() {
 
-        // Felhasználó módosítása
-        $this->updateUser();
+        DB::transaction(function () {
+
+            // Aktuális szerepkörök lekérdezése
+            $current_roles = UserRole::where("user_id", $this->id)->pluck("role_id")->toArray();
+
+            // Új szerepkörök lekérdezése
+            $new_roles = array_map("intval", $this->roles);
+
+            // Törlendő szerepkörök
+            $remove_roles = array_values(array_diff($current_roles, $new_roles));
+
+            // Ezen szerepkörök törlése
+            for ($i=0; $i<count($remove_roles); $i++) {
+                UserRole::where("user_id", $this->id)->where("role_id", $remove_roles[$i])->delete();
+            }
+
+            // Hozzáadandó szererkörök
+            $add_roles = array_values(array_diff($new_roles, $current_roles));
+
+            // Ezen szerepkörök hozzáadása
+            for ($i=0; $i<count($add_roles); $i++) {
+                $user_role = new UserRole();
+                $user_role->user_id = $this->id;
+                $user_role->role_id = $add_roles[$i];
+                $user_role->save();
+            }
+
+            // Felhasználó módosítása
+            $this->updateUser();
+        });
     }
 
     // Felhasználó módosítása
