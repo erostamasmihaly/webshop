ba339fca11c4cfa0dd1fe7d89c76f3d9477e34e5
sa_et <eros.tamas@miskolcholding.hu>
2023-08-17 12:42:01 +0200

    Leaflet II.

--- public/js/sajat/admin_shop_edit.js
+++ public/js/sajat/admin_shop_edit.js
@@ -1,13 +1,51 @@
 $(function () {
-    console.log("Okay");
 
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    // GPS szélesség és hosszúság lekérdezése
+    var latitude = $("input[name='latitude']").val();
+    var longitude = $("input[name='longitude']").val();
 
+    // Leaflet pont létrehozása ezen két koordinátából
+    var point = L.latLng(latitude, longitude);
+
+    // Térkép létrehozása, aminek ezen pont lesz a középpontja
+    var map = L.map('map').setView(point, 13);
+
+    // OpenStreetMap réteg létrehozása
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+    // Jelölő létrehozása
+    var marker = L.marker(point, {draggable:'true'}).addTo(map);
+
+    // Jelölő mozgatása után
+    marker.on('dragend', function(event){
+
+        // Térképjelölő lekérdezése
+        var marker = event.target;
+
+        // Új pozíció lekérdezése
+        var position = marker.getLatLng();
+
+        // Szélesség és hosszúság lekérdezése és kerekítése
+        var latitude = position.lat.toFixed(4);
+        var longitude = position.lng.toFixed(4);
+
+        // Beviteli mezők módosítása
+        $("input[name='latitude']").val(latitude);
+        $("input[name='longitude']").val(longitude);
+
+        // Új pont létrehozása
+        var point = L.latLng(latitude, longitude); 
+
+        // Jelölő áthelyezése
+        marker.setLatLng(point,{draggable:'true'});
+
+        // Térkép mozgatása erre a pontra
+        map.panTo(point);
+
+      });
+
+      
+
 });
