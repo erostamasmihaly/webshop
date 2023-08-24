027f8744a4833b6b03c5e36c346d173770d3daad
sa_et <eros.tamas@miskolcholding.hu>
2023-08-24 16:36:01 +0200

    Fényképek kezelése III.

--- app/Http/Services/ProductUpdate.php
+++ app/Http/Services/ProductUpdate.php
@@ -3,18 +3,20 @@
 namespace App\Http\Services;
 
 use App\Http\Requests\ProductUpdateRequest;
+use App\Models\Image;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 
 class ProductUpdate
 {
     public $name;
-    private $id, $summary, $body, $price, $category_id, $quantity, $active, $vat, $discount, $shop_id, $unit_id;
+    private $id, $temporary_id, $summary, $body, $price, $category_id, $quantity, $active, $vat, $discount, $shop_id, $unit_id;
 
     // Adatok lekérdezése
     public function __construct(ProductUpdateRequest $productUpdateRequest)
     {
         $this->id = $productUpdateRequest->id;
+        $this->temporary_id = $productUpdateRequest->temporary_id;
         $this->name = $productUpdateRequest->name;
         $this->summary = $productUpdateRequest->summary;
         $this->body = $productUpdateRequest->body;
@@ -37,8 +39,10 @@ class ProductUpdate
             // Létrehozás vagy megnyitás
             if ($this->id==0) {
                 $product = new Product();
+                $new = true;
             } else {
                 $product = Product::find($this->id);
+                $new = false;
             }
 
             // További adatok módosítása
@@ -56,6 +60,23 @@ class ProductUpdate
             
             // Mentés
             $product->save();
+
+            // Ha új termék lett létrehozva
+            if ($new) {
+
+                // Módosítani a hozzárendelt képek termék azonosítóját: ideiglenes > állandó
+                Image::where('product_id', $this->temporary_id)->update(['product_id' => $product->id]);
+
+                // Régi könyvtár
+                $old_dir = public_path('images/products/'.$this->temporary_id);
+
+                // Új könyvtár
+                $new_dir = public_path('images/products/'.$product->id);
+
+                // Átnevezni a fájlokat tartalmazó könyvtárat
+                rename($old_dir, $new_dir);
+                
+            }
 
         });
     }
