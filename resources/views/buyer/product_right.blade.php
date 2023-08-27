705dbc03727dea95175de19bbde8792f0209850b
sa_et <eros.tamas@miskolcholding.hu>
2023-08-27 16:05:36 +0200

    Mini pontosítás

--- resources/views/buyer/product_right.blade.php
+++ resources/views/buyer/product_right.blade.php
@@ -5,7 +5,7 @@
     @if($product->discount)
         <div class="card p-2 bg-danger text-white text-center mb-2">
             <div class="text-decoration-line-through">{{ numformat_with_unit($product->brutto_price,'Ft') }}</div>
-            <div class="fw-bold">{{ numformat_with_unit($product->discount,'%') }} akció!</div>
+            <div class="fw-bold">{{ numformat_with_unit($product->discount,'%') }} kedvezmény!</div>
         </div>
     @endif
     <div class="card p-2 bg-success text-white text-center mb-2">
