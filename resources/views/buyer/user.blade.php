619817620c7f8d847c202783f55cc1d9c959f44a
sa_et <eros.tamas@miskolcholding.hu>
2023-08-31 11:35:14 +0200

    Profil oldal II.

--- resources/views/buyer/user.blade.php
+++ resources/views/buyer/user.blade.php
@@ -1,9 +1,12 @@
 @extends('buyer.layouts.app')
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        @include('buyer.user_left')
-        @include('buyer.user_right')
-    </div>
+    <form action="{{ route('buyer_user') }}" method="POST" enctype="multipart/form-data">
+        <input type="hidden" name="id" value="{{ $user->id }}"/>    
+        <div class="row justify-content-center">
+            @include('buyer.user_left')
+            @include('buyer.user_right')
+        </div>
+    </form>
 </div>
 @endsection
--- resources/views/buyer/user_left.blade.php
+++ resources/views/buyer/user_left.blade.php
@@ -3,5 +3,88 @@
         <div class="bg-info bg-gradient text-dark text-center mb-2">
             <h1>{{ $user->surname }} {{ $user->forename }}</h1>
         </div>
+        <div class="bg-info bg-gradient text-dark text-center mb-2 fw-bold">
+            Fiók adatok
+        </div>
+        <div class="row mb-2">
+            <div class="col-sm-3 fw-bold">Felhasználói név</div>
+            <div class="col-sm-9 fw-bold">
+                <input type="text" class="form-control" name="name" value="{{ old('name',$user->name) }}"/>
+                @if ($errors->has('name'))
+				    <div class="invalid-feedback d-block">{{ $errors->first('name') }}</div>
+			    @endif
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-sm-3 fw-bold">Vezetéknév</div>
+            <div class="col-sm-9 fw-bold">
+                <input type="text" class="form-control" name="surname" value="{{ old('surname',$user->surname) }}"/>
+                @if ($errors->has('surname'))
+				    <div class="invalid-feedback d-block">{{ $errors->first('surname') }}</div>
+			    @endif
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-sm-3 fw-bold">Keresztnév</div>
+            <div class="col-sm-9 fw-bold">
+                <input type="text" class="form-control" name="forename" value="{{ old('forename',$user->forename) }}"/>
+                @if ($errors->has('forename'))
+				    <div class="invalid-feedback d-block">{{ $errors->first('forename') }}</div>
+			    @endif
+            </div>
+        </div>
+        <div class="bg-info bg-gradient text-dark text-center mb-2 fw-bold">
+            Személyes adatok
+        </div>
+        <div class="row mb-2">
+            <div class="col-sm-3 fw-bold">Ország</div>
+            <div class="col-sm-9 fw-bold">
+                <input type="text" class="form-control" name="country" value="{{ old('country',$user->country) }}"/>
+                @if ($errors->has('country'))
+				    <div class="invalid-feedback d-block">{{ $errors->first('country') }}</div>
+			    @endif
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-sm-3 fw-bold">Területi egység</div>
+            <div class="col-sm-9 fw-bold">
+                <input type="text" class="form-control" name="state" value="{{ old('state',$user->state) }}"/>
+                @if ($errors->has('state'))
+				    <div class="invalid-feedback d-block">{{ $errors->first('state') }}</div>
+			    @endif
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-sm-3 fw-bold">Irányítószám</div>
+            <div class="col-sm-9 fw-bold">
+                <input type="text" class="form-control" name="zip" value="{{ old('zip',$user->zip) }}"/>
+                @if ($errors->has('zip'))
+				    <div class="invalid-feedback d-block">{{ $errors->first('zip') }}</div>
+			    @endif
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-sm-3 fw-bold">Település</div>
+            <div class="col-sm-9 fw-bold">
+                <input type="text" class="form-control" name="city" value="{{ old('city',$user->city) }}"/>
+                @if ($errors->has('city'))
+				    <div class="invalid-feedback d-block">{{ $errors->first('city') }}</div>
+			    @endif
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-sm-3 fw-bold">Utca, házszám...</div>
+            <div class="col-sm-9 fw-bold">
+                <input type="text" class="form-control" name="address" value="{{ old('address',$user->address) }}"/>
+                @if ($errors->has('address'))
+				    <div class="invalid-feedback d-block">{{ $errors->first('address') }}</div>
+			    @endif
+            </div>
+        </div>
+        <div class="bg-dark p-3">
+            <div class="submit">
+                <button type="submit" class="btn btn-primary">Mentés</button>            
+            </div>
+        </div>
     </div>
 </div>
