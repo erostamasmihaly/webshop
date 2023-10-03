b9a390976f7b17404180dfa1c05496a8131c83cc
sa_et <eros.tamas@miskolcholding.hu>
2023-10-03 11:00:45 +0200

    Query > JavaScript VI.

--- public/js/own/buyer_cart.js
+++ public/js/own/buyer_cart.js
@@ -1,65 +1,49 @@
 $(function () {
 
     // Plusz gomb megnyomása
-    plus_buttons = document.querySelectorAll(".plus");
-    if (plus_buttons!=null) {
-        for (var i=0; i<plus_buttons.length; i++) {
-            plus_buttons[i].addEventListener("click", function() {
+    [...document.querySelectorAll(".plus")].map(element => element.addEventListener("click", function() {
         
-                // Termék azonosító és méret azonosító lekérdezése
-                product_id = this.closest(".product").getAttribute("product_id");
-                size_id = this.closest(".product").getAttribute("size_id");
+        // Termék azonosító és méret azonosító lekérdezése
+        product_id = this.closest(".product").getAttribute("product_id");
+        size_id = this.closest(".product").getAttribute("size_id");
         
-                // Módosítás elküldése
-                send_change(product_id, size_id, 1);
+        // Módosítás elküldése
+        send_change(product_id, size_id, 1);
            
-            });
-        }
-    }
+    }));
 
     // Minusz gomb megnyomása
-    minus_buttons = document.querySelectorAll(".minus");
-    if (minus_buttons!=null) {
-        for (var i=0; i<minus_buttons.length; i++) {
-            minus_buttons[i].addEventListener("click", function() {
+    [...document.querySelectorAll(".minus")].map(element => element.addEventListener("click", function() {
         
-                // Termék azonosító és méret azonosító lekérdezése
-                product_id = this.closest(".product").getAttribute("product_id");
-                size_id = this.closest(".product").getAttribute("size_id");
+        // Termék azonosító és méret azonosító lekérdezése
+        product_id = this.closest(".product").getAttribute("product_id");
+        size_id = this.closest(".product").getAttribute("size_id");
 
-                // Mennyiség lekérdezése
-                quantity = $(this).closest(".product").find(".quantity").html();
+        // Mennyiség lekérdezése
+        quantity = $(this).closest(".product").find(".quantity").html();
 
-                // Ha még nem 0
-                if (quantity > 0) {
+        // Ha még nem 0
+        if (quantity > 0) {
 
-                    // Módosítás elküldése
-                    send_change(product_id, size_id, -1);
-                }
-           
-            });
+            // Módosítás elküldése
+            send_change(product_id, size_id, -1);
         }
-    }
+    }));
 
     // Törlés gomb megnyomása
-    delete_buttons = document.querySelectorAll(".delete");
-    if (delete_buttons!=null) {
-        for (var i=0; i<delete_buttons.length; i++) {
-            delete_buttons[i].addEventListener("click", function() {
+    [...document.querySelectorAll(".delete")].map(element => element.addEventListener("click", function() {
         
-                // Termék azonosító és méret azonosító lekérdezése
-                product_id = this.closest(".product").getAttribute("product_id");
-                size_id = this.closest(".product").getAttribute("size_id");
+        // Termék azonosító és méret azonosító lekérdezése
+        product_id = this.closest(".product").getAttribute("product_id");
+        size_id = this.closest(".product").getAttribute("size_id");
 
-                // Mennyiség lekérdezése
-                quantity = $(this).closest(".product").find(".quantity").html();
+        // Mennyiség lekérdezése
+        quantity = $(this).closest(".product").find(".quantity").html();
 
-                // Módosítás elküldése
-                send_change(product_id, size_id, -1*quantity);
+        // Módosítás elküldése
+        send_change(product_id, size_id, -1*quantity);
                 
-            });
-        }
-    }
+    }));
 
     // Módosítás elküldése
     function send_change(product_id, size_id, quantity) {
