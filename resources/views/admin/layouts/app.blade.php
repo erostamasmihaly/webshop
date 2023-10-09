a7308ee4ec1cf8e9897a92cb516cc4e8f0839c8e
sa_et <eros.tamas@miskolcholding.hu>
2023-10-09 13:48:36 +0200

    Fejléc színek

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -11,7 +11,7 @@
         @show
     </head>
     <body>
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-danger">
             <a class="navbar-brand ps-3" href="{{ route('home') }}">{{ config('app.name', 'ETM Webshop') }}</a>
             @if(isset(Auth::user()->name))
                 <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
