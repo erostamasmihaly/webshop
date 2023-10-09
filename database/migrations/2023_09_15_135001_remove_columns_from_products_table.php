51edee9007d9cad1d20f59e492ebfe17ca2e48cd
sa_et <eros.tamas@miskolcholding.hu>
2023-10-09 16:53:50 +0200

    BUG II.

--- database/migrations/2023_09_15_135001_remove_columns_from_products_table.php
+++ database/migrations/2023_09_15_135001_remove_columns_from_products_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn('unit_id');
+            //$table->dropColumn('unit_id');
+            $table->dropConstrainedForeignId('unit_id');
             $table->dropColumn('category_id');
         });
     }
