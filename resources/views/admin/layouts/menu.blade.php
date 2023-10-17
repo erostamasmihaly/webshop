d52e16b854cdb3821d045d56b23e65d50886639f
sa_et <eros.tamas@miskolcholding.hu>
2023-10-17 11:23:19 +0200

    Menü javítás

--- resources/views/admin/layouts/menu.blade.php
+++ resources/views/admin/layouts/menu.blade.php
@@ -1,7 +1,7 @@
 <a class="nav-link @if(Request::is('admin/user*')) active @endif" href="{{ route('admin_user') }}">
     <div class="sb-nav-link-icon"><i class="fas fa-users"></i></div>Felhasználók
 </a>
-<a class="nav-link @if(Request::is('admin/category*')) active @endif" href="{{ route('admin_category_group') }}">
+<a class="nav-link @if(Request::is('admin/group*')) active @endif" href="{{ route('admin_category_group') }}">
     <div class="sb-nav-link-icon"><i class="fas fa-folder-tree"></i></div>Kategória csoportok
 </a>
 <a class="nav-link @if(Request::is('admin/shop*')) active @endif" href="{{ route('admin_shop') }}">
