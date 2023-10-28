81e00efa43b8ae7f9e6cbb004fad87cdbe916495
sa_et <eros.tamas@miskolcholding.hu>
2023-10-28 08:03:36 +0200

    Csillag javítás

--- resources/js/pages/Product.vue
+++ resources/js/pages/Product.vue
@@ -16,9 +16,9 @@
             </table>
             <div class="bg-primary text-light p-2 fw-bold">
                 Értékelések
-                <span class="float-end">
-                    <i class="fa-solid fa-star" v-for="star in ratings.stars"></i>
-                    <i class="fa-regular fa-star" v-for="star in 5-(ratings.stars)"></i>
+                <span v-if="ratings.length>0" class="float-end fw-bold">
+                    <span v-for="star in ratings.stars">&#9733;</span>
+                    <span v-for="star in 5-(ratings.stars)">&#9734;</span>
                 </span>
             </div>
             <div v-if="ratings.items.length == 0" class="alert alert-warning mt-2" role="alert">
@@ -32,9 +32,9 @@
                             fontStyle: item.moderated == 0 ? 'italic' : 'normal' 
                         }">
                             <span class="fw-bold">{{ item.title }}</span>
-                            <span class="float-end">
-                                <i class="fa-solid fa-star" v-for="star in item.stars"></i>
-                                <i class="fa-regular fa-star" v-for="star in 5-(item.stars)"></i>
+                            <span class="float-end fw-bold">
+                                <span v-for="star in item.stars">&#9733;</span>
+                                <span v-for="star in 5-(item.stars)">&#9734;</span>
                             </span>
                             <div>
                                 <span v-html="item.body"></span>
