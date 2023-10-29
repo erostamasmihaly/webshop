750458692b76f06219eff5fd11bac70fd5ac6da6
sa_et <eros.tamas@miskolcholding.hu>
2023-10-29 16:58:46 +0100

    REACT Main

--- resources/js/components/Main.jsx
+++ resources/js/components/Main.jsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const RenderMenu = props => {
+    const menu = [
+        { link: "list", name: "Termékek listája", btnclass: "btn-secondary"},
+        { link: "user", name: "Felhasználó adatai", btnclass: "btn-secondary"},
+        { link: "payed", name: "Eddigi vásárlások", btnclass: "btn-secondary"},
+        { link: "cart", name: "Kosár tartalma", btnclass: "btn-secondary"},
+        { link: "notification", name: "Értesítések", btnclass: "btn-secondary"},
+    ];
+  
+    return (
+        <div className="row">
+        {menu.map((item,index) => (
+            <div className="col-sm-3" key={index}>
+                <button className={"btn "+item.btnclass+" w-100 m-1"}>{ item.name }</button>
+            </div>
+        ))}
+        </div>
+    );
+};
+
 function Main() {
     return (
-        <div className="container">
-            <div className="row justify-content-center">
-                <div className="col-md-8">
-                    <div className="card">
-                        <div className="card-header">Example Component</div>
-                        <div className="card-body">I'm an example component!</div>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <h1>REACT által készített alkalmazások</h1>
+            <h2>A szürkével jelzett részek még mincsenek készen!</h2>
         </div>
     );
 }
@@ -24,6 +38,7 @@ if (document.getElementById('main')) {
     Index.render(
         <React.StrictMode>
             <Main/>
+            <RenderMenu/>
         </React.StrictMode>
     )
 }
