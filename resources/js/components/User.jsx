06bb05ecfd2f070f54842088e94d42b86309e1fc
sa_et <eros.tamas@miskolcholding.hu>
2023-11-01 15:15:35 +0100

    React, de nem megy... II.

--- resources/js/components/User.jsx
+++ resources/js/components/User.jsx
@@ -9,13 +9,13 @@ const RenderUser = () => {
 
     // Elemek lekérdezése
     useEffect(() => {
+
         getFetch('/api/user', setUser);
+
     }, []);
 
-    console.log(user);
-    
-    /*const forceChange = () => {
-        [...document.querySelectorAll("form input")].map(element => {
+    const forceChange = (form) => {
+        [...form.elements].map(element => {
             const name = element.name;
             const value = element.value;
             setInputs(values => ({...values, [name]: value}));
@@ -24,68 +24,62 @@ const RenderUser = () => {
 
     const [inputs, setInputs] = useState(user);
 
-    const handleChange = (event) => {
-        const name = event.target.name;
-        const value = event.target.value;
-        setInputs(values => ({...values, [name]: value}));
-    }
-
     const handleSubmit = (event) => {
         event.preventDefault();
-        forceChange();
+        forceChange(event.target);
         console.log(inputs);
-    } */
+    }
 
     // Visszatérés a táblázattal
     return (
         <form onSubmit={handleSubmit}>
-            <input type="hidden" name="id" defaultValue={user.id || 0} onChange={handleChange}/>
+            <input type="hidden" name="id" defaultValue={user.id}/>
             <div className="row mb-2">
                 <div className="col-sm-3">Felhasználói név</div>
                 <div className="col-sm-9">
-                    <input type="text" className="form-control" name="name" defaultValue={user.name || ''} onChange={handleChange}/>
+                    <input type="text" className="form-control" name="name"  defaultValue={user.name}/>
                 </div>
             </div>
             <div className="row mb-2">
                 <div className="col-sm-3">Vezetéknév</div>
                 <div className="col-sm-9">
-                    <input type="text" className="form-control" name="surname" defaultValue={user.surname || ''} onChange={handleChange}/>
+                    <input type="text" className="form-control" name="surname" defaultValue={user.surname}/>
                 </div>
             </div>
             <div className="row mb-2">
                 <div className="col-sm-3">Keresztnév</div>
                 <div className="col-sm-9">
-                    <input type="text" className="form-control" name="forename" defaultValue={user.forename || ''} onChange={handleChange}/>
+                    <input type="text" className="form-control" name="forename" defaultValue={user.forename}/>
                 </div>
             </div>
             <div className="row mb-2">
                 <div className="col-sm-3">Ország</div>
                 <div className="col-sm-9">
-                    <input type="text" className="form-control" name="country" defaultValue={user.country || ''} onChange={handleChange}/>
+                    <input type="text" className="form-control" name="country" defaultValue={user.country}/>
                 </div>
             </div>
             <div className="row mb-2">
                 <div className="col-sm-3">Területi egység</div>
                 <div className="col-sm-9">
-                    <input type="text" className="form-control" name="state" defaultValue={user.state || ''} onChange={handleChange}/>
+                    <input type="text" className="form-control" name="state" defaultValue={user.state}/>
                 </div>
             </div>
             <div className="row mb-2">
                 <div className="col-sm-3">Irányítószám</div>
                 <div className="col-sm-9">
-                    <input type="text" className="form-control" name="zip" defaultValue={user.zip || ''} onChange={handleChange}/>
+                    <input type="text" className="form-control" name="zip" defaultValue={user.zip}/>
                 </div>
             </div>
             <div className="row mb-2">
                 <div className="col-sm-3">Település</div>
                 <div className="col-sm-9">
-                    <input type="text" className="form-control" name="city" defaultValue={user.city || ''} onChange={handleChange}/>
+                    <input type="text" className="form-control" name="city" defaultValue={user.city}/>
                 </div>
             </div>
             <div className="row mb-2">
                 <div className="col-sm-3">Utca, házszám...</div>
                 <div className="col-sm-9">
-                    <input type="text" className="form-control" name="address" defaultValue={user.address || ''} onChange={handleChange}/>
+                    <input type="text" className="form-control" name="address" defaultValue={user.address}/>
                 </div>
             </div>  
             <button className="btn btn-primary">Mentés</button>
